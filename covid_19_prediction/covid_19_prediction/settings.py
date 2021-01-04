eafc9e50de06d6d75e415f7aa2358732c11be1a0
Nejc Hauptman <nejc.hauptman@student.um.si>
2021-01-04 21:32:52 +0100

    fetching / scraping and organizing data from covid19.sledilnik.org into csvfile

--- covid_19_prediction/covid_19_prediction/settings.py
+++ covid_19_prediction/covid_19_prediction/settings.py
@@ -7,6 +7,7 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
+from shutil import which
 BOT_NAME = 'covid_19_prediction'
 
 SPIDER_MODULES = ['covid_19_prediction.spiders']
@@ -37,34 +38,34 @@ ROBOTSTXT_OBEY = True
 #TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
+# DEFAULT_REQUEST_HEADERS = {
 #   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 #   'Accept-Language': 'en',
-#}
+# }
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
+# SPIDER_MIDDLEWARES = {
 #    'covid_19_prediction.middlewares.Covid19PredictionSpiderMiddleware': 543,
-#}
+# }
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
+# DOWNLOADER_MIDDLEWARES = {
 #    'covid_19_prediction.middlewares.Covid19PredictionDownloaderMiddleware': 543,
-#}
+# }
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
+# EXTENSIONS = {
 #    'scrapy.extensions.telnet.TelnetConsole': None,
-#}
+# }
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
+# ITEM_PIPELINES = {
 #    'covid_19_prediction.pipelines.Covid19PredictionPipeline': 300,
-#}
+# }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
--- covid_19_prediction/covid_19_prediction/spiders/Covid19.py
+++ covid_19_prediction/covid_19_prediction/spiders/Covid19.py
@@ -1,5 +1,84 @@
 import scrapy
+import json
+from pprint import pprint
+
 
 class Covid19Spider(scrapy.Spider):
     name = 'covid19'
-    
+    allowed_domains = ['covid-19.sledilnik.org']
+    start_urls = ['https://api.sledilnik.org/api/stats']
+
+    def parse(self, response):
+        params = {}
+        resp = json.loads(response.body)
+        for i in resp:
+            if((i['year'] == 2020)):
+                params['year'] = i['year']
+                params['dayFromStart'] = i['dayFromStart']
+                params['year'] = i['year']
+                params['day'] = i['day']
+                params['month'] = i['month']
+                params['LJ regija'] = i['statePerRegion']['lj']
+                params['po regija'] = i['statePerRegion']['po']
+                params['kr regija'] = i['statePerRegion']['kr']
+                params['ce regija'] = i['statePerRegion']['ce']
+                params['foreign regija'] = i['statePerRegion']['foreign']
+                params['nm regija'] = i['statePerRegion']['nm']
+                params['kk regija'] = i['statePerRegion']['kk']
+                params['mb regija'] = i['statePerRegion']['mb']
+                params['ms regija'] = i['statePerRegion']['ms']
+                params['za regija'] = i['statePerRegion']['za']
+                params['ng regija'] = i['statePerRegion']['ng']
+                params['kp regija'] = i['statePerRegion']['kp']
+                for l, k in i['tests']['positive'].items():
+                    if(l == 'toDate'):
+                        params['tested_poz_to_date'] = k
+                    else:
+                        params['tested_poz_today'] = k
+                for b, c in i['tests']['performed'].items():
+                    if(b == 'toDate'):
+                        params['tested_to_date'] = c
+                    else:
+                        params['tested_today'] = c
+                for j in i['statePerAgeToDate']:
+                    if(j.get('ageFrom') == 15):
+                        params['age 15-24'] = j.get('allToDate')
+                        params['age 15-24 FEMALE'] = j.get('femaleToDate')
+                        params['age 15-24 MEN'] = j.get('maleToDate')
+                    if(j.get('ageFrom') == 25):
+                        params['age 25-34'] = j.get('allToDate')
+                        params['age 25-34 FEMALE'] = j.get(
+                            'femaleToDate')
+                        params['age 25-34 MEN'] = j.get('maleToDate')
+                    if(j.get('ageFrom') == 35):
+                        params['age 35-44'] = j.get('allToDate')
+                        params['age 35-44 FEMALE'] = j.get(
+                            'femaleToDate')
+                        params['age 35-44 MEN'] = j.get('maleToDate')
+                    if(j.get('ageFrom') == 45):
+                        params['age 45-54'] = j.get('allToDate')
+                        params['age 45-54 FEMALE'] = j.get(
+                            'femaleToDate')
+                        params['age 45-54 MEN'] = j.get('maleToDate')
+                    if(j.get('ageFrom') == 55):
+                        params['age 55-64'] = j.get('allToDate')
+                        params['age 55-64 FEMALE'] = j.get(
+                            'femaleToDate')
+                        params['age 55-64 MEN'] = j.get('maleToDate')
+                    if(j.get('ageFrom') == 65):
+                        params['age 65-74'] = j.get('allToDate')
+                        params['age 65-74 FEMALE'] = j.get(
+                            'femaleToDate')
+                        params['age 65-74 MEN'] = j.get('maleToDate')
+                    if(j.get('ageFrom') == 75):
+                        params['age 75-84'] = j.get('allToDate')
+                        params['age 75-84 FEMALE'] = j.get(
+                            'femaleToDate')
+                        params['age 75-84 MEN'] = j.get('maleToDate')
+                    if(j.get('ageFrom') == 85):
+                        params['age 85+'] = j.get('allToDate')
+                        params['age 85+ FEMALE'] = j.get(
+                            'femaleToDate')
+                        params['age 85+ MEN'] = j.get('maleToDate')
+
+            yield params
